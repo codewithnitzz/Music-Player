160015b9be912aea42de5555d808815443c204ba
NITIN VERMA <77920339+codewithnitzz@users.noreply.github.com>
2021-07-06 01:32:30 +0530

    Successfully Coommited

---
+++ js/music-list.js
@@ -0,0 +1,80 @@
+let allMusic = [
+  {
+    name: "Starboy",
+    artist: "The Weeknd Ft Daft Punk",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Lean On",
+    artist: "Major Lazor",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Trampoline",
+    artist: "Zayn Ft. Shaed",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Brown Munde",
+    artist: "AP Dhillon, Gurinder Singh",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Liggi",
+    artist: "Ritviz",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Save Your Tears",
+    artist: "The Weeknd",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Love Me Harder",
+    artist: "Ariana Grande, The Weeknd",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "Waalian",
+    artist: "Harnoor",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Born To Shine",
+    artist: "Diljit Dosanjh",
+    img: "music-9",
+    src: "music-9"
+  },
+  {
+    name: "The Nights",
+    artist: "Avicii",
+    img: "music-10",
+    src: "music-10"
+  },
+  {
+    name: "Peaches",
+    artist: "Justin Bieber Ft Quavo",
+    img: "music-11",
+    src: "music-11"
+  },
+  {
+    name: "Dusk Till Dawn",
+    artist: "Zayn Ft Sia",
+    img: "music-12",
+    src: "music-12"
+  },
+  {
+    name: "Tu Aake Dekhle",
+    artist: "King",
+    img: "music-13",
+    src: "music-13"
+  }
+];
---
+++ music-list.js
@@ -0,0 +1,32 @@
+let allMusic = [
+    { name: 'Starboy',
+       artist: 'The Weeknd Ft Daft Punk',
+       img: "img1",
+       src: "Starboy" 
+    },
+
+    { name: 'Lean on',
+       artist: 'Major Lazor',
+       img: "img2",
+       src: "LO" 
+    },
+
+    { name: 'Trampoline',
+       artist: 'Zayn Ft Shaed',
+       img: "img3",
+       src: "Trampoline" 
+    },
+
+    { name: 'Brown Munde',
+       artist: 'AP Dhillon, Gurinder Singh',
+       img: "img4",
+       src: "BM" 
+    },
+
+    { name: 'Liggi',
+       artist: 'Ritviz',
+       img: "img5",
+       src: "Liggi" 
+    }
+
+]
